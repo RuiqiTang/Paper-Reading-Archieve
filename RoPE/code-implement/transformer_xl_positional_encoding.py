e628f1410ab209862ebb561cd88598ac63376edd
Ruiqi Tang <RuiQiTarng@gmail.com>
2025-05-03 10:51:33 +0800

    Update transformer_xl_positional_encoding.py

--- RoPE/code-implement/transformer_xl_positional_encoding.py
+++ RoPE/code-implement/transformer_xl_positional_encoding.py
@@ -1,4 +1,9 @@
 '''
-Ref: https://github.com/kimiyoung/transformer-xl
+Ref: https://github.com/kimiyoung/transformer-xl/blob/master/tf/model.py
+核心代码解读: 
+1. https://zhuanlan.zhihu.com/p/74485142
+2. https://www.cnblogs.com/shiyublog/p/11236212.html#_labelTop
 '''
+
+
 
